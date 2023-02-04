d9a778ce3c56b4aa6f6f243d4d0213048bfe6b33
Paul Luong <paulluong@Pauls-MacBook-Pro.local>
2023-02-03 23:20:45 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
