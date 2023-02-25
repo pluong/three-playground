1c150b3b0d0b9661b850267d29307511767dcf40
Paul Luong <paulluong@Pauls-MacBook-Pro.local>
2023-02-24 22:30:51 -0500

    fix scaling issue; add hyperlink and cursor behavior

---
+++ src/scratch.js
@@ -0,0 +1,38 @@
+//--------- cube ------------------
+
+    // const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // // const geometry = new THREE.SphereGeometry(5, 2);
+    // const material = new THREE.MeshBasicMaterial({ color: 0x00ffe0 });
+    // const cube = new THREE.Mesh(geometry, material);
+    // scene.add(cube);
+
+    // camera.position.z = 2;
+
+    //------- line -------
+    // //create a blue LineBasicMaterial
+    // const lineMaterial = new THREE.LineBasicMaterial({ color: 0xfffff });
+
+    // const points = [];
+    // points.push(new THREE.Vector3(- 10, 0, 0));
+    // points.push(new THREE.Vector3(0, 10, 0));
+    // points.push(new THREE.Vector3(10, 0, 0));
+
+    // const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    // const line = new THREE.Line(lineGeometry, lineMaterial);
+
+    // scene.add(line);
+
+
+
+    //--------- Lighting
+    {
+        // const skyColor = 0xB1E1FF;  // light blue
+        // const groundColor = 0xB97A20;  // brownish orange
+        // const intensity = 0.6;
+        // const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+        // scene.add(light);
+    }
+
+    // cube.rotation.x += 0.01;
+        // cube.rotation.y += 0.01;
